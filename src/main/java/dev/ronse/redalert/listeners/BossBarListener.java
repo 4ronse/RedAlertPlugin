3f6e7eb3696bc4b9d6c319ca8c64f0d4bc9b4665
Ron Segal <4ronse@gmail.com>
2023-10-17 20:52:54 +0300

    Made some changes to the BossBarListener

--- src/main/java/dev/ronse/redalert/listeners/BossBarListener.java
+++ src/main/java/dev/ronse/redalert/listeners/BossBarListener.java
@@ -4,35 +4,45 @@ import dev.ronse.redalert.RedAlert;
 import dev.ronse.redalert.orefalerts.OrefAlert;
 import dev.ronse.redalert.util.Format;
 import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BossBarListener extends BaseOrefAlertListener {
     public BossBarListener(RedAlert plugin) {
         super(plugin);
     }
 
+    private final Map<Long, BossBar> bossbars = new HashMap<>();
+
     @Override
     public void onOrefAlert(OrefAlert alert) {
-        String format = Format.format(RedAlert.config.notifiers.bossbarNotifier.format, alert);
-
         BossBar bar = BossBar.bossBar(
-                Format.colorize_legacy(format),
+                Format.colorize_legacy(
+                        Format.format(RedAlert.config.notifiers.bossbarNotifier.format, alert)
+                ),
                 BossBar.MAX_PROGRESS,
                 RedAlert.config.notifiers.bossbarNotifier.color,
-                BossBar.Overlay.PROGRESS);
+                BossBar.Overlay.NOTCHED_20
+        );
 
-        plugin.getServer().getOnlinePlayers().forEach(p -> {
+        for(var p : Bukkit.getOnlinePlayers()) {
+            if(bossbars.containsKey(alert.getId())) p.hideBossBar(bossbars.get(alert.getId()));
+            bossbars.put(alert.getId(), bar);
+
             p.showBossBar(bar);
 
-            BukkitRunnable disableBar = new BukkitRunnable() {
+            // Hide bossbar
+            new BukkitRunnable() {
                 @Override
-                public void run() {
-                    p.hideBossBar(bar);
-                }
-            };
-
-            disableBar.runTaskLater(plugin, 20L * RedAlert.config.notifiers.bossbarNotifier.duration);
-
-        });
+                public void run() { p.hideBossBar(bar); }
+            }.runTaskLater(plugin, 20L * RedAlert.config.notifiers.bossbarNotifier.duration);
+        }
     }
 }
